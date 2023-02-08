e03022020582cd1e84d66ed759e2aec7faa3dfce
UM_ENVY\sn <sny.umd@gmail.com>
2023-02-08 18:01:58 +0900

    init追加

--- MauiPost/MauiPost/MauiPost/App.xaml.cs
+++ MauiPost/MauiPost/MauiPost/App.xaml.cs
@@ -1,11 +1,13 @@
-﻿namespace MauiPost;
+﻿using MauiPost.Classes;
+
+namespace MauiPost;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-
+		ClsCommon.Init();
 		MainPage = new AppShell();
 	}
 }
---
+++ MauiPost/MauiPost/MauiPost/Classes/ClsCommon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using MauiCtrl;
+
+namespace MauiPost.Classes
+{
+    class ClsCommon
+    {
+        public const string FileNameBody = "Body.json"; 
+        public static string CurrentDir = SystemCtrl.GetCurrentDir();
+        public static bool BlJson = false;
+
+        public static JsonNode JnBodysValue;
+
+        public static void Init()
+        {
+            BlJson = FileCtrl.FindFile(CurrentDir, FileNameBody);
+            if (!BlJson)
+            {
+                BlJson = FileCtrl.CreateFile(CurrentDir, FileNameBody);
+                if(BlJson) Console.WriteLine(FileCtrl.WriteFile(CurrentDir, FileNameBody, "{\r\n\t\"value\":\"{}\"\r\n}", false));
+            }
+            Console.WriteLine(FileCtrl.ReadFile(CurrentDir, FileNameBody));
+        }
+    }
+}
